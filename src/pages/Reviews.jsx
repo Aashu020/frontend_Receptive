bc03b3aad4b3430f975b36ccb2e536ea59476db2
Rishabh-prime <rishabh1616thakur@gmail.com>
2025-09-30 12:31:54 +0530

    changes in review card

--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -298,7 +298,7 @@ function Reviews() {
       </div>
 
       {/* Reviews Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 items-start">
         {reviews.map((review) => (
           <div
             key={review.id}
